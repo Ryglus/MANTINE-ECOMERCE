5277140dc582987d57f36cbd635ac87b3f0e03fa
Ryglus <jirka16.kucera@seznam.cz>
2024-10-18 13:45:44 +0200

    +Footer

--- web/src/components/ui/page-footer.component.tsx
+++ web/src/components/ui/page-footer.component.tsx
@@ -1,59 +1,34 @@
-import {ActionIcon, Anchor, Center, Container, Grid, Group, Image, useMantineTheme} from '@mantine/core';
+import {ActionIcon, Container, Flex, Group, Image, useMantineTheme} from '@mantine/core';
 import {IconBrandFacebook, IconBrandInstagram, IconBrandTwitter} from '@tabler/icons-react';
 
 export default function PageFooter() {
     const theme = useMantineTheme();
+
     return (
-        <footer className="w-full transition-all duration-300 py-2.5"
-                style={{
-                    backgroundColor: theme.colors.primary[9],
-                    marginTop:"auto"
-                }}>
-            <Container size={"xl"}>
-                <Grid align="center">
-                    <Grid.Col span={4}>
-                        <Group dir="column" align="flex-start" gap="xs">
-                            <Anchor href="/about" c="gray" size="sm">
-                                About Us
-                            </Anchor>
-                            <Anchor href="/contact" c="gray" size="sm">
-                                Contact
-                            </Anchor>
-                            <Anchor href="/privacy" c="gray" size="sm">
-                                Privacy Policy
-                            </Anchor>
-                            <Anchor href="/terms" c="gray" size="sm">
-                                Terms & Conditions
-                            </Anchor>
-                        </Group>
-                    </Grid.Col>
-
-                    <Grid.Col span={4}>
-                        <Center>
-                            <Image
-                                style={{ height: '70px' }}
-                                src="/logo2edit.webp"
-                                alt="VelvetCove Logo"
-                                fit="contain"
-                                className="object-contain"
-                            />
-                        </Center>
-                    </Grid.Col>
-
-                    <Grid.Col span={4}>
-                        <Group p="right" gap="xs">
-                            <ActionIcon size="lg" component="a" href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <footer style={{ backgroundColor: theme.colors.primary[9], marginTop: 'auto' }}>
+            <Container size="xl" py={"sm"}>
+                <Flex justify={"space-between"} >
+                    <Image
+                        style={{ height: '70px' }}
+                        src="/logo2edit.webp"
+                        alt="VelvetCove Logo"
+                        fit="contain"
+                        className="object-contain"
+                    />
+                    <Group gap="xl" align="center">
+                        <Group gap="xs">
+                            <ActionIcon size="lg" component="a" href="/" target="_blank" rel="noopener noreferrer">
                                 <IconBrandFacebook size={24} color="white" />
                             </ActionIcon>
-                            <ActionIcon size="lg" component="a" href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                            <ActionIcon size="lg" component="a" href="/" target="_blank" rel="noopener noreferrer">
                                 <IconBrandTwitter size={24} color="white" />
                             </ActionIcon>
-                            <ActionIcon size="lg" component="a" href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                            <ActionIcon size="lg" component="a" href="/" target="_blank" rel="noopener noreferrer">
                                 <IconBrandInstagram size={24} color="white" />
                             </ActionIcon>
                         </Group>
-                    </Grid.Col>
-                </Grid>
+                    </Group>
+                </Flex>
             </Container>
         </footer>
     );
